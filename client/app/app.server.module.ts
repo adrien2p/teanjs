790ac8a16417cd27cbdf3bd5310c04f01b8dcd18
adrien de Peretti <adrien.deperetti@gmail.com>
2019-09-17 17:18:23 +0200

    Project(Client) Re-organize

--- client/app/app.server.module.ts
+++ client/app/app.server.module.ts
@@ -5,7 +5,7 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UniversalInterceptor } from './interceptors/universal.interceptor';
+import { UniversalInterceptor } from './common/interceptors/universal.interceptor';
 
 @NgModule({
     imports: [
